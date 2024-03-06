76954460d1cb3df0309564745574cec8f123fdeb
leeseongbok <lsb@j-sol.co.kr>
2024-03-06 18:55:38 +0900

    feat: 아이디 중복 조회 추가

--- api/src/main/kotlin/kr/co/jsol/api/controller/AccountController.kt
+++ api/src/main/kotlin/kr/co/jsol/api/controller/AccountController.kt
@@ -2,6 +2,7 @@ package kr.co.jsol.api.controller
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
 import kr.co.jsol.common.domain.AccountAuthority
 import kr.co.jsol.common.jwt.JwtService
 import kr.co.jsol.domain.account.application.AccountService
@@ -12,6 +13,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -21,6 +23,7 @@ import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api/v1/accounts")
+@Tag(name = "계정", description = "계정 관련 API")
 class AccountController(
     private val service: AccountService,
     private val jwtService: JwtService,
@@ -53,13 +56,13 @@ class AccountController(
     @Operation(summary = "중복 조회")
     @PreAuthorize(AccountAuthority.ROLECHECK.HasAnyRole)
     @SecurityRequirement(name = "Bearer Authentication")
-    @GetMapping("exists")
+    @GetMapping("exists/{id}")
     @ResponseStatus(HttpStatus.OK)
     fun existsId(
-        @AuthenticationPrincipal
-        userDetails: UserDetailsImpl,
+        @PathVariable
+        id: String,
     ): Boolean {
-        return service.existsById(userDetails.id)
+        return service.existsById(id)
     }
 
     @Operation(summary = "내 정보 조회")
