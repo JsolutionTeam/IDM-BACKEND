a2940207dfa74f07863415d597f5d67bc939254a
leeseongbok <lsb@j-sol.co.kr>
2024-03-06 17:43:05 +0900

    feat: 공시지원금 수정 DTO 미사용 속성 제거

--- domain/src/main/kotlin/kr/co/jsol/domain/devicesubsidy/application/dto/UpdateDeviceSubsidyDto.kt
+++ domain/src/main/kotlin/kr/co/jsol/domain/devicesubsidy/application/dto/UpdateDeviceSubsidyDto.kt
@@ -1,8 +1,6 @@
 package kr.co.jsol.domain.devicesubsidy.application.dto
 
 import io.swagger.v3.oas.annotations.media.Schema
-import kr.co.jsol.domain.telecom.entity.enums.DiscountType
-import kr.co.jsol.domain.telecom.entity.enums.OpenType
 import javax.validation.constraints.Min
 import javax.validation.constraints.NotNull
 
@@ -13,17 +11,8 @@ data class UpdateDeviceSubsidyDto(
     @field:Schema(description = "회사 지원금 아이디")
     val id: Long,
 
-    @field:Schema(description = "개통 유형", implementation = OpenType::class)
-    val openType: OpenType? = null,
-
-    @field:Schema(description = "선약/공시 할인 유형", implementation = DiscountType::class)
-    val discountType: DiscountType? = null,
-
     @field:Schema(description = "공시 지원금 가격")
     val price: Long? = null,
-
-    @field:Schema(description = "idm - shop idx")
-    val shopId: Long? = null,
 
     @field:Schema(description = "idm - telecom idx")
     val telecomId: Long? = null,
