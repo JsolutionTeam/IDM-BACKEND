fb3f815f635f3cbae5a325261c633b702581492e
leeseongbok <lsb@j-sol.co.kr>
2024-03-12 13:59:54 +0900

    feat: api 사용 방식에 맞게 수정

--- domain/src/main/kotlin/kr/co/jsol/domain/telecomdevice/infrastructure/dto/TelecomDeviceFactory.kt
+++ domain/src/main/kotlin/kr/co/jsol/domain/telecomdevice/infrastructure/dto/TelecomDeviceFactory.kt
@@ -13,7 +13,13 @@ class TelecomDeviceFactory(
     fun create(telecomDevice: TelecomDevice): TelecomDeviceDto {
 //        println("shopApiUrl = ${shopApiUrl}")
         // imageUrl를 풀 경로로 만들기 위해 shopApiUrl을 붙여줌
-        telecomDevice.imageUrl = "$shopApiUrl/api/v1/local-files/${telecomDevice.imageUrl}"
+        val imageUrl = telecomDevice.imageUrl
+
+        // 만약 http를 포함하는 경로라면 이미지 경로를 그대로 사용
+        if (imageUrl.contains("http")) {
+            return TelecomDeviceDto(telecomDevice)
+        }
+        telecomDevice.imageUrl = "$shopApiUrl/api/v1/local-files?filename=$imageUrl"
         return TelecomDeviceDto(telecomDevice)
     }
 }
